d21660a0be20f58a96b76f87781ff9888ecea187
Akaza Store <akazastore@vercel.com>
2026-05-22 20:58:46 +0700

    fix: dynamically set storage path to /tmp/storage in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -17,3 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// Override storage path for Vercel Serverless environment
+if (is_writable('/tmp')) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app;
