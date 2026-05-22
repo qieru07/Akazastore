a64489704e1c5146f2164f19e5c22b558ca5e7aa
Akaza Store <akazastore@vercel.com>
2026-05-23 02:06:30 +0700

    Fix admin logout issues by adding GET fallback and CSRF exemption

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ $app = Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        $middleware->validateCsrfTokens(except: [
+            'logout',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
